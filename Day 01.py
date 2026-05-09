49bdc8fefc83f1c9dcbeabb08edc41802bc8add5
Bikrom-roy-4561 <bikromroy0711@gmail.com>
2026-05-10 01:47:19 +0600

    add new

---
+++ Day 01.py
@@ -0,0 +1,26 @@
+# How to print "Hello World" in Python
+print('Hello World')
+
+# How to print multiple items in a single line
+print("My favourite movie is", "Fast and Furious", "Wolf ot wall street", "The Dark Knight")
+
+
+# Variables in Python
+name = 'Bikrom Adatya Roy'
+age = 24
+cgpa = 3.30
+print ("My name is .name, I am {age} years old and my cgpa is {cgpa}")
+print ("My name is {}, I am {} years old and my cgpa is {}".format(name, age, cgpa))
+
+# way2 : 
+print(name)
+print(age)
+print(cgpa)
+
+#Data Types in Python
+name = "Bikrom Adatya Roy" # String
+phn_num = 1234567890 # Integer
+cgpa = 3.30 # Float
+is_student = True # Boolean
+
+
