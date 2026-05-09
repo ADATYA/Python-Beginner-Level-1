0ca8288d5bbd6fee17a20395910c09e92c514f00
BIKROM <97549431+ADATYA@users.noreply.github.com>
2026-05-10 01:39:14 +0600

    Add files via upload

---
+++ Day 01.py
@@ -0,0 +1,20 @@
+# How to print "Hello World" in Python
+print('Hello World')
+
+# How to print multiple items in a single line
+print("My favourite movie is", "Fast and Furious", "Wolf ot wall street", "The Dark Knight")
+
+
+# Variables in Python
+name = 'Bikrom Adatya Roy'
+age = 24
+cgpa = 3.30
+print ("My name is .name, I am {age} years old and my cgpa is {cgpa}")
+print ("My name is {}, I am {} years old and my cgpa is {}".format(name, age, cgpa))
+
+# way2 : 
+print(name)
+print(age)
+print(cgpa)
+
+
